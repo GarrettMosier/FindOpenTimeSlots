3f525d28964a3100bd98bb24ef828efc8bb85db0
Garrett Mosier <x3g2@nordstrom.com>
2016-08-19 09:08:27 -0700

    Remove invalid results

--- Main.hs
+++ Main.hs
@@ -5,24 +5,29 @@ type DayPeriod = TimeRange
 type OpenSlots = [TimeRange]
 type Meetings = [TimeRange]
 
-testRange = [TimeRange 10 12, TimeRange 1 4, TimeRange 19 40, TimeRange 8 9]
+testRange :: Meetings
+testRange = [TimeRange 10 12, TimeRange 1 4, TimeRange 19 40, TimeRange 8 9, TimeRange 0 22]
+
+testDayRange :: DayPeriod
 testDayRange = TimeRange 0 20
 
 
 findOpenPeriods :: DayPeriod -> Meetings -> OpenSlots
-findOpenPeriods dayRange@(TimeRange startTime endTime) allMeetings@(meeting : remainingMeetings) = reverse openSlots
-                where openSlots = findOpenPeriodsHelper dayRange cleanMeetings [] startTime
+findOpenPeriods dayRange@(TimeRange dayStart dayEnd) allMeetings = reverse $ filter validTimeRange openSlots
+                where openSlots = findOpenPeriodsHelper dayRange cleanMeetings [] dayStart
                       cleanMeetings = filter withinDay $ sort allMeetings
-                      withinDay (TimeRange start end) = end > startTime && start < endTime -- Double check semantics for being in a day
-                      
+                      withinDay (TimeRange start end) = start < dayEnd && end > dayStart -- Double check semantics for being in a day
+                      validTimeRange (TimeRange start end) = start < end
 
 findOpenPeriodsHelper :: DayPeriod -> Meetings -> OpenSlots -> Int -> OpenSlots
 findOpenPeriodsHelper _ [] openSlots _ = openSlots
-findOpenPeriodsHelper dayRange@(TimeRange dayStartTime dayEndTime) (meeting@(TimeRange meetingStart meetingEnd) : remainingMeetings) openSlots meetingRangeEnd = remainingOpenMeetings
+findOpenPeriodsHelper dayRange ((TimeRange meetingStart meetingEnd) : remainingMeetings) openSlots meetingRangeEnd = remainingOpenMeetings
                 where remainingOpenMeetings = findOpenPeriodsHelper dayRange remainingMeetings newOpenSlots newMeetingRangeEnd
                       toAppend = meetingEnd > meetingRangeEnd
-                      newMeetingRangeEnd = meetingEnd
+                      newMeetingRangeEnd = max meetingEnd meetingRangeEnd
                       openSlot = TimeRange meetingRangeEnd meetingStart
                       newOpenSlots = if toAppend then openSlot : openSlots else openSlots
 
+
+main :: IO ()
 main = print $ findOpenPeriods testDayRange testRange
