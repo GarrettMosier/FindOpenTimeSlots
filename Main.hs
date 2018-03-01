f795a7a3d885cae12eb8e7eb5f3bb9af2148abe8
Garrett Mosier <x3g2@nordstrom.com>
2018-03-01 12:59:07 -0800

    Removed Int as Time

--- Main.hs
+++ Main.hs
@@ -1,9 +1,10 @@
 import Data.List
 
-data TimeRange = TimeRange Int Int deriving (Show, Eq, Ord)
+data TimeRange = TimeRange Time Time deriving (Show, Eq, Ord)
 type DayPeriod = TimeRange
 type OpenSlots = [TimeRange]
 type Meetings = [TimeRange]
+type Time = Int
 
 testRange :: Meetings
 testRange = [TimeRange 10 12, TimeRange 1 4, TimeRange 18 19, TimeRange 8 9, TimeRange 4 25, TimeRange (-5) 1]
@@ -19,7 +20,7 @@ findOpenPeriods dayRange@(TimeRange dayStart dayEnd) allMeetings = reverse $ fil
                       withinDay (TimeRange start end) = start < dayEnd && end > dayStart -- Double check semantics for being in a day
                       validTimeRange (TimeRange start end) = start < end
 
-findOpenPeriodsHelper :: DayPeriod -> Meetings -> OpenSlots -> Int -> OpenSlots
+findOpenPeriodsHelper :: DayPeriod -> Meetings -> OpenSlots -> Time -> OpenSlots
 findOpenPeriodsHelper (TimeRange _ dayEnd) [] openSlots lastMeetingEnd = if lastMeetingEnd < dayEnd then (TimeRange lastMeetingEnd dayEnd) : openSlots else openSlots
 findOpenPeriodsHelper dayRange ((TimeRange meetingStart meetingEnd) : remainingMeetings) openSlots meetingRangeEnd = remainingOpenMeetings
                 where remainingOpenMeetings = findOpenPeriodsHelper dayRange remainingMeetings newOpenSlots newMeetingRangeEnd
