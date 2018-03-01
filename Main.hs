93a55c8e329ff30ed6a4116db1837827ab8a1917
Garrett Mosier <x3g2@nordstrom.com>
2018-03-01 13:43:32 -0800

    Moved range check further in stack

--- Main.hs
+++ Main.hs
@@ -26,21 +26,19 @@ validTimeRange (TimeRange start end) = start < end
 
 findOpenPeriods :: DayPeriod -> Meetings -> OpenSlots
 findOpenPeriods dayRange@(TimeRange dayStart dayEnd) allMeetings = reverse $ filter validTimeRange openSlots
-                where openSlots = findOpenPeriodsHelper dayRange meetingsDuringDay [] dayStart
-                      meetingsDuringDay = filter withinDay $ sort allMeetings
-                      withinDay (TimeRange start end) = start < dayEnd && end > dayStart
+                where openSlots = findOpenPeriodsHelper dayRange (sort allMeetings) [] dayStart
 
 
 findOpenPeriodsHelper :: DayPeriod -> Meetings -> OpenSlots -> Time -> OpenSlots
 findOpenPeriodsHelper (TimeRange _ dayEnd) [] openSlots lastMeetingEnd = if lastMeetingEnd < dayEnd
                                                                              then (TimeRange lastMeetingEnd dayEnd) : openSlots
                                                                              else openSlots
-findOpenPeriodsHelper dayRange ((TimeRange meetingStart meetingEnd) : remainingMeetings) openSlots meetingRangeEnd = remainingOpenMeetings
+findOpenPeriodsHelper dayRange@(TimeRange _ dayEnd) ((TimeRange meetingStart meetingEnd) : remainingMeetings) openSlots meetingRangeEnd = remainingOpenMeetings
                 where remainingOpenMeetings = findOpenPeriodsHelper dayRange remainingMeetings newOpenSlots newMeetingRangeEnd
-                      toAppend = meetingEnd > meetingRangeEnd
-                      newMeetingRangeEnd = max meetingEnd meetingRangeEnd
+                      toAppendToOpenSlots = meetingEnd > meetingRangeEnd
+                      newMeetingRangeEnd = if meetingEnd > dayEnd then dayEnd else max meetingEnd meetingRangeEnd 
                       openSlot = TimeRange meetingRangeEnd meetingStart
-                      newOpenSlots = if toAppend then openSlot : openSlots else openSlots
+                      newOpenSlots = if toAppendToOpenSlots then openSlot : openSlots else openSlots
 
 
 main :: IO ()
