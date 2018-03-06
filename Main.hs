98a54f5e741117b87ac5617a9508d18e9cae1d35
Garrett Mosier <x3g2@nordstrom.com>
2018-03-06 15:41:21 -0800

    Added ability to schedule by day

--- Main.hs
+++ Main.hs
@@ -1,44 +1,52 @@
 import Data.List
 
-newtype Time   = Time Int deriving (Show, Eq, Ord)
-data TimeRange = TimeRange Time Time deriving (Show, Eq, Ord)
-type DayPeriod = TimeRange
-type OpenSlots = [TimeRange]
-type Meetings  = [TimeRange]
+newtype Time a   = Time a deriving (Show, Eq, Ord)
+data TimeRange a = TimeRange (Time a) (Time a) deriving (Show, Eq, Ord)
+type DayPeriod a = TimeRange a
+type OpenSlots a = [TimeRange a]
+type Meetings  a = [TimeRange a]
+
+data Day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday deriving (Ord, Eq, Show)
 
 
-testRangeOne :: Meetings
+testRangeOne :: Meetings Int
 testRangeOne = [TimeRange (Time 10) (Time 12), TimeRange (Time 1) (Time 4), TimeRange (Time 18) (Time 19), TimeRange (Time 8) (Time 9), TimeRange (Time 4) (Time 25), TimeRange (Time (-5)) (Time 1)]
 
-testRangeTwo :: Meetings
+testRangeTwo :: Meetings Int
 testRangeTwo = [TimeRange (Time 5) (Time 15), TimeRange (Time 1) (Time 2), TimeRange (Time 18) (Time 19), TimeRange (Time 10) (Time 13)]
 
-testRangeThree :: Meetings
+testRangeThree :: Meetings Int
 testRangeThree = [TimeRange (Time 2) (Time 4), TimeRange (Time 3) (Time 5)]
 
-testDayRange :: DayPeriod
+testDayRange :: DayPeriod Int
 testDayRange = TimeRange (Time 0) (Time 24)
+
+testWeekRange :: DayPeriod Day
+testWeekRange = TimeRange (Time Sunday) (Time Saturday)
+
+testWeekOne :: Meetings Day
+testWeekOne = [TimeRange (Time Sunday) (Time Tuesday), TimeRange (Time Thursday) (Time Friday)]
 
 
 -- Ensures time range makes sense
-validTimeRange :: TimeRange -> Bool
+validTimeRange :: Ord a => TimeRange a -> Bool
 validTimeRange (TimeRange start end) = start < end && start /= end
 
 
 -- Ensures meeting time is within the course of a day
-clampTimeToDay :: DayPeriod -> TimeRange -> TimeRange
+clampTimeToDay :: Ord a => DayPeriod a -> TimeRange a -> TimeRange a
 clampTimeToDay (TimeRange dayStart dayEnd) (TimeRange start end) = (TimeRange (max start dayStart) (min end dayEnd))
 
 
 -- Finds all time during day where a meeting isn't scheduled
-findOpenPeriods :: DayPeriod -> Meetings -> OpenSlots
+findOpenPeriods :: Ord a => DayPeriod a -> Meetings a -> OpenSlots a
 findOpenPeriods dayRange@(TimeRange dayStart _) allMeetings = reverse openSlots
                 where openSlots = findOpenPeriodsHelper dayRange validMeetings [] dayStart
                       validMeetings = filter validTimeRange $ sort remappedMeetings
                       remappedMeetings = map (clampTimeToDay dayRange) allMeetings
 
 
-findOpenPeriodsHelper :: DayPeriod -> Meetings -> OpenSlots -> Time -> OpenSlots
+findOpenPeriodsHelper :: Ord a => DayPeriod a -> Meetings a -> OpenSlots a -> Time a -> OpenSlots a
 findOpenPeriodsHelper (TimeRange _ dayEnd) [] openSlots lastMeetingEnd = if lastMeetingEnd < dayEnd
                                                                              then (TimeRange lastMeetingEnd dayEnd) : openSlots
                                                                              else openSlots
@@ -55,3 +63,4 @@ main = do
   print $ findOpenPeriods testDayRange testRangeOne
   print $ findOpenPeriods testDayRange testRangeTwo
   print $ findOpenPeriods testDayRange testRangeThree
+  print $ findOpenPeriods testWeekRange testWeekOne
