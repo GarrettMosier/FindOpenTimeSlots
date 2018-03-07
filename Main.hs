1c4c59f73a9c55b45e417da59cfe1a2d51f825f5
Garrett Mosier <x3g2@nordstrom.com>
2018-03-07 09:11:07 -0800

    Moved common functions and types into seperate file

--- Main.hs
+++ Main.hs
@@ -1,24 +1,4 @@
-import Data.List
-
-data PointInTime = PointInTime Day Int deriving (Eq, Ord)
-newtype Time a   = Time a deriving (Eq, Ord)
-data TimeRange a = TimeRange (Time a) (Time a) deriving (Eq, Ord)
-type BookablePeriod a = TimeRange a
-type OpenSlots a = [TimeRange a]
-type Meetings  a = [TimeRange a]
-
-data Day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday deriving (Ord, Eq, Show)
-
-
-instance Show PointInTime where
-  show (PointInTime day time) = show day ++ " at " ++ show time
-
-instance Show a => Show (TimeRange a) where
-  show (TimeRange beginning end) = show beginning ++ " to " ++ show end
-
-instance Show a => Show (Time a) where
-  show (Time x) = show x
-  
+import Lib
 
 testRangeOne :: Meetings Int
 testRangeOne = [TimeRange (Time 10) (Time 12), TimeRange (Time 1) (Time 4), TimeRange (Time 18) (Time 19), TimeRange (Time 8) (Time 9), TimeRange (Time 4) (Time 25), TimeRange (Time (-5)) (Time 1)]
@@ -49,34 +29,6 @@ testFullWeekRange = TimeRange (Time (PointInTime Sunday 0)) (Time (PointInTime S
 
 
 
--- Ensures time range makes sense
-validTimeRange :: Ord a => TimeRange a -> Bool
-validTimeRange (TimeRange start end) = start < end && start /= end
-
-
--- Ensures meeting time is within the course of a day
-clampTimeToDay :: Ord a => BookablePeriod a -> TimeRange a -> TimeRange a
-clampTimeToDay (TimeRange dayStart dayEnd) (TimeRange start end) = (TimeRange (max start dayStart) (min end dayEnd))
-
-
--- Finds all time during day where a meeting isn't scheduled
-findOpenPeriods :: Ord a => BookablePeriod a -> Meetings a -> OpenSlots a
-findOpenPeriods dayRange@(TimeRange dayStart _) allMeetings = reverse openSlots
-                where openSlots = findOpenPeriodsHelper dayRange validMeetings [] dayStart
-                      validMeetings = filter validTimeRange $ sort remappedMeetings
-                      remappedMeetings = map (clampTimeToDay dayRange) allMeetings
-
-
-findOpenPeriodsHelper :: Ord a => BookablePeriod a -> Meetings a -> OpenSlots a -> Time a -> OpenSlots a
-findOpenPeriodsHelper (TimeRange _ dayEnd) [] openSlots lastMeetingEnd = if lastMeetingEnd < dayEnd
-                                                                             then (TimeRange lastMeetingEnd dayEnd) : openSlots
-                                                                             else openSlots
-findOpenPeriodsHelper dayRange ((TimeRange meetingStart meetingEnd) : remainingMeetings) openSlots meetingRangeEnd = foundOpenSlots
-                where foundOpenSlots = findOpenPeriodsHelper dayRange remainingMeetings newOpenSlots newMeetingRangeEnd
-                      toAppendToOpenSlots = meetingStart > meetingRangeEnd
-                      newMeetingRangeEnd = max meetingEnd meetingRangeEnd 
-                      openSlot = TimeRange meetingRangeEnd meetingStart
-                      newOpenSlots = if toAppendToOpenSlots then openSlot : openSlots else openSlots
 
 
 main :: IO ()
